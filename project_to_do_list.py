7e9a258f63fcb61c05540fdea7268e824b04a092
RTDavi <davi.gmramos@gmail.com>
2024-04-27 17:30:26 -0300

    Option 1 atualization

--- project_to_do_list.py
+++ project_to_do_list.py
@@ -9,7 +9,7 @@ print("WELCOME TO THE DIGITAL TO-DO-LIST")
 while True:
     print("--------------------------------------")
     print("WHAT DO YOU WANT TO DO IN THE LIST?")
-    print("1. ")
+    print("TYPE [1] TO ADD SOMETHING IN THE LIST ")
     print("TYPE [2] TO SEE THE LIST")
     print("TYPE [3] TO REMOVE SOMETHING IN THE LIST")
     print("TYPE [4] TO EDIT SOMETHING IN THE LIST")
@@ -22,11 +22,21 @@ while True:
         print('TYPE A REAL NUMBER')
         continue
 
-    if int(answer) == 1:
-        with open("to_do_list.txt", "w") as archive:
+    elif int(answer) == 1:
+        with open("to_do_list.txt", "a") as archive:
             writing_the_list = input("What you will put in the list?")
-            archive.write(f'1. {writing_the_list}')
+            archive.write(f'\n{writing_the_list}\n')
+
     
-    if int(answer) == 5:
+    elif int(answer) == 2:
+        with open("to_do_list.txt", "r") as archive:
+            print(archive.read())
+
+    elif int(answer) == 3:
+        input("WHAT TASK DO YOU WANT TO REMOVE?")
+
+    elif int(answer) == 5:
+        print("GOODBYE!! \n [APPLICATION ENDED]")
         sys.exit()
+        
         
